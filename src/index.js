43a0322c78f31d44c74a90b45dcafc7dad897850
lev <lleevv2020@gmail.com>
2024-01-11 21:09:00 +0400

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
 import { rootReducer } from './reducers'
 
-const store = createStore(rootReducer)
+/*function logger(store) {
+  return function (next) {
+    return function (action) {
+      const result = next(action)
+      console.log('middleware', store.getState())
+      return result
+    }
+  }
+}*/
+const logger = (store) => (next) => (action) => {
+  const result = next(action)
+  console.log('middleware', store.getState())
+  return result
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 console.log(store.getState())
